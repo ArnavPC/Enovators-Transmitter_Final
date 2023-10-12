778530687d63681e72310ad26e7b88056ac71e61
ArnavPC <134386538+ArnavPC@users.noreply.github.com>
2023-10-12 18:32:19 +0530

    inverted controls

--- main.ts
+++ main.ts
@@ -25,17 +25,17 @@ function back () {
 function ForwardBack () {
     if (pins.analogReadPin(AnalogPin.P1) > 1020 && pins.analogReadPin(AnalogPin.P1) < 1024) {
         basic.pause(10)
-        countdownforward += 1
-        if (countdownforward == 1) {
-            forward()
-            countdownforward = 0
-        }
-    } else if (pins.analogReadPin(AnalogPin.P1) < 300) {
-        basic.pause(10)
         countdown += 1
         if (countdown == 1) {
             back()
             countdown = 0
+        }
+    } else if (pins.analogReadPin(AnalogPin.P1) < 300) {
+        basic.pause(10)
+        countdownforward += 1
+        if (countdownforward == 1) {
+            forward()
+            countdownforward = 0
         }
     }
 }
@@ -98,6 +98,19 @@ input.onButtonPressed(Button.B, function () {
 function LeftRight () {
     if (pins.analogReadPin(AnalogPin.P2) > 800) {
         if (work == 0) {
+            radio.sendMessage(RadioMessage.left)
+            work = 1
+            _break = 0
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
+        }
+    } else if (pins.analogReadPin(AnalogPin.P2) < 300) {
+        if (work == 0) {
             radio.sendMessage(RadioMessage.right)
             work = 1
             _break = 0
@@ -109,19 +122,6 @@ function LeftRight () {
                 . . # . .
                 `)
             ctgagain = 0
-        }
-    } else if (pins.analogReadPin(AnalogPin.P2) < 300) {
-        if (work == 0) {
-            radio.sendMessage(RadioMessage.left)
-            work = 1
-            _break = 0
-            basic.showLeds(`
-                . . # . .
-                . # . . .
-                # # # # #
-                . # . . .
-                . . # . .
-                `)
         }
     }
 }
@@ -145,8 +145,8 @@ buttonClicks.onButtonHeld(buttonClicks.AorB.A, function () {
 })
 let ctgagain = 0
 let powerspeed = 0
-let countdown = 0
 let countdownforward = 0
+let countdown = 0
 let _break = 0
 let work = 0
 let speed = 0
